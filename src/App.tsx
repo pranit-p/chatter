8c20e99a7f2b6139d0b29c49b30c777b53c7f1dc
pranit-p <pranit.pawar@medly.com>
2022-01-14 12:11:01 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+
+import { ChatContainer, MessageInput } from "@chatscope/chat-ui-kit-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatContainer>
+        <div>
+          <MessageInput />
+          <button>Custom button</button>
+        </div>
+      </ChatContainer>
     </div>
   );
 }
---
+++ src/declarations.d.ts
@@ -0,0 +1,2 @@
+// declare module "@chatscope/chat-ui-kit-styles/dist/default/styles.css";
+// declare module "@chatscope/chat-ui-kit-react";
